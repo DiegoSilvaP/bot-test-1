752bdaa5a1221b346fa840d2478ecacf61e85945
Diego Silva <alienware.matrix@gmail.com>
2019-12-03 14:20:11 -0600

    first commit- initial development

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=botwithsearchapi-test-1-Web-Deploy.pubxml -p:Password=SJb0BrmYraKQlq0P4AM48Af1sptmdqSgh7BTNF2BuhTyvnHm4bZ3y7sWX7Du
+
